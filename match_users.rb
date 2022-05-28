4088aa78bb20027a6262d24c82f96077399f22b1
Eamon Cullen <eamonc123@gmail.com>
2022-05-28 19:33:42 +0200

    Find matches on any columns
    Added loop to check for matches on any column and print results
    
    TODO generate and append user ID to hashes as k:v pairs and write array to new csv

--- match_users.rb
+++ match_users.rb
@@ -7,11 +7,44 @@ chosenCSV = "input" + gets.chomp + ".csv"
 # searchTerm = gets.chomp.downcase
 
 
-data = CSV.parse(File.read(chosenCSV), headers: true, header_converters: :symbol).map(&:to_h)
+data = CSV.parse(File.read(chosenCSV), headers: true).map(&:to_h)
 puts data
 
 puts "----------------------"
 
 
-output = (data[1].keys & data[6].keys).select { |k| data[1][k] == data[6][k] }
-#puts output
+# output = (data[1].keys & data[6].keys).select { |k| data[1][k] == data[6][k] }
+
+# for i in 0..data.length
+#   for j in i..data.length
+#     # puts data[j + 1]
+#     output = (data[j].keys & data[j+1].keys).select { |k| data[j][k] == data[j+1][k] }
+#     puts data[j]
+#     puts data[j+1]
+#     puts output
+#   end
+#   puts "*****"
+# end
+
+for i in 0..data.length-2
+  for j in i+1..data.length-1
+    output = (data[i].keys & data[j].keys).select { |k| data[i][k] == data[j][k] }
+    puts "We are comparing this row..."
+    puts data[i]
+    puts "With this row!"
+    puts data[j]
+
+    puts "Here's the output:"
+    puts output
+    puts "*****"
+
+    if output.include? "Email"
+      puts "Match!"
+      puts "---"
+    else
+      puts "No match :("
+      puts "---"
+    end
+
+  end
+end
